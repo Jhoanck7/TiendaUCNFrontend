ed000f3706abaaec4e7d5d3aa173f9c88181f064
kdevmilk <69746299+Belacqua1@users.noreply.github.com>
2025-11-30 03:01:32 -0300

    feat(page): refactor product detail page to use async/await and simplify component structure

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,45 +1,14 @@
-"use client";
+import { productService } from "@/services/product-service";
+import ProductDetailView from "@/views/app/products/[id]/ProductDetailView";
 
-import { useEffect, useState } from "react";
-import { getProductById, Product } from "@/services/products";
-import { useCartStore } from "@/src/store/cart";
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
 
-interface Props {
-  params: { id: string };
-}
+  const product = await productService.getProductDetail(id);
 
-export default function ProductDetailPage({ params }: Props) {
-  const [product, setProduct] = useState<Product | null>(null);
-  const [loading, setLoading] = useState(true);
-  const addItem = useCartStore((state) => state.addItem);
-
-  useEffect(() => {
-    getProductById(params.id)
-      .then((r) => setProduct(r.data))
-      .catch((err) => console.error(err))
-      .finally(() => setLoading(false));
-  }, [params.id]);
-
-  if (loading) return <p>Cargando producto...</p>;
-  if (!product) return <p>Producto no encontrado.</p>;
-
-  return (
-    <div>
-      <h1>{product.name}</h1>
-      <p>Precio: {product.price}</p>
-
-      <button
-        onClick={() =>
-          addItem({
-            id: product.id,
-            name: product.name,
-            price: product.price,
-            quantity: 1,
-          })
-        }
-      >
-        Agregar al carrito
-      </button>
-    </div>
-  );
+  return <ProductDetailView product={product} />;
 }
